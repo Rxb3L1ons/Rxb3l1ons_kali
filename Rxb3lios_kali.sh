3fa55a82e72307dfee80f9e6cd86f85f124624bb
Rxb3l1ons <ariel19atr@gmail.com>
2026-02-11 20:25:05 -0300

    Interfaz Dragón y Nombre agregados

--- Rxb3lios_kali.sh
+++ Rxb3lios_kali.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 clear
-echo -e "\e[1;35m[!] SISTEMA RXB3LIOS INICIADO...\e[0m"
+
+# --- INTERFAZ DRAGÓN RXB3L1ONS ---
+echo -e "\e[1;36m"
+echo "             ___====-_  _--==___"
+echo "       _--^^^  //      \\    ^^^--_"
+echo "    _-^       // (    ) \\       ^-_"
+echo "   -         //  |\^^/|  \\         -"
+echo " _/         //   | (0) |   \\         \_"
+echo " |         //    |  ||  |    \\         |"
+echo " |        //     \  ||  /     \\        |"
+echo " |       //       \ || /       \\       |"
+echo " \_     //         \||/         \\     _/"
+echo "   -   //           ||           \\   -"
+echo "    \_//            ||            \\_/"
+echo "      ||            ||             ||"
+echo -e "\e[1;35m"
+echo "  ____  __  ______ _____  _      _  ____  _   _  ____  "
+echo " |  _ \ \ \/ / __ |____ | |    / |/ __ \| \ | |/ ___| "
+echo " | |_) | \  /|  _ \ / / | |    | | |  | |  \| | \___ \ "
+echo " |  _ <  /  \| |_) / /  | |___ | | |__| | |\  |  ___) |"
+echo " |_| \_\/_/\_\____/ /___|_____||_|\____/|_| \_| |____/ "
+echo -e "\e[1;36m"
+echo "             SYSTEM ONLINE - BY RXB3L1ONS"
+echo -e "\e[0m"
+
 sleep 2
+
+# --- INICIO DE COMANDOS ---
 echo -ne "\e[1;32mkali@kali:~$\e[0m termux-setup-storage\n"
 termux-setup-storage && sleep 1.5
 echo -ne "\e[1;32mkali@kali:~$\e[0m pkg install wget -y\n"
